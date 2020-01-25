ecf4332aba877b236ec49afb725062ed176254b4
Dero <106393@gmail.com>
2020-01-25 14:08:22 +0400

    Updates

---
+++ precache-manifest.050b5492a1c527bf74e884e696785095.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "262dfe5da79cb8ae9f026698781c6aeb",
+    "url": "/react-redux-test-min-c/index.html"
+  },
+  {
+    "revision": "bef1303503e57cc9da4a",
+    "url": "/react-redux-test-min-c/static/css/main.92ac3958.chunk.css"
+  },
+  {
+    "revision": "7b9e0ecf7525e4b1f8bb",
+    "url": "/react-redux-test-min-c/static/js/2.513fb1ad.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-redux-test-min-c/static/js/2.513fb1ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bef1303503e57cc9da4a",
+    "url": "/react-redux-test-min-c/static/js/main.6752d33a.chunk.js"
+  },
+  {
+    "revision": "508386aca6b41ae75854",
+    "url": "/react-redux-test-min-c/static/js/runtime-main.c4d45328.js"
+  }
+]);
